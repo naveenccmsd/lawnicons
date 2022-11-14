1913737969e15027a35c23db41f199c78968555b
Naveen <naveen.ccmsd@gmail.com>
2022-11-14 10:06:06 +0530

    revert hilt version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("org.jetbrains.kotlin.jvm") version "1.7.20" apply false
     id("com.sergei-lapin.napt") version "1.17" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.44.1" apply false
     id("com.google.android.gms.oss-licenses-plugin") version "0.10.5" apply false
     id("org.jmailen.kotlinter") version "3.12.0" apply false
 }
