a5c953d734fd508dbd8c3587815e26677a54c368
Naveen <naveen.ccmsd@gmail.com>
2022-11-17 10:48:06 +0530

    Bump versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "app.lawnchair.lawnicons"
         minSdk = 26
         targetSdk = 31
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
         vectorDrawables.useSupportLibrary = true
     }
 
